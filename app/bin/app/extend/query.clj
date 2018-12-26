9086d7d09496a0426106e7c4486904707c92c18c
rifdyf <rifdyf@gmail.com>
2018-12-26 11:44:10 +0700

    query select + form + post type

---
+++ app/bin/app/extend/query.clj
@@ -0,0 +1,18 @@
+(ns app.extend.query
+  (:require [app.layout :as layout]
+            [app.db.core :as db]
+            [compojure.core :refer [defroutes ANY]]
+            [ring.util.http-response :as response]
+            [clojure.java.io :as io]))
+
+(defn query-page [ad_org_id]
+  (layout/render "query.html"
+    {:my-var1 (db/get-organization_mapping {:ad_org_id ad_org_id})}))
+
+(defn parse-int [number-string]
+  (try (Integer/parseInt number-string)
+    (catch Exception e nil)))
+
+(defroutes query-routes
+  (ANY "/query" [ad_org_id] (query-page (parse-int ad_org_id)))
+  )
---
+++ app/bin/app/handler.clj
@@ -0,0 +1,31 @@
+(ns app.handler
+  (:require [app.middleware :as middleware]
+            [app.layout :refer [error-page]]
+            [app.routes.home :refer [home-routes]]
+            [compojure.core :refer [routes wrap-routes]]
+            [ring.util.http-response :as response]
+            [compojure.route :as route]
+            [app.env :refer [defaults]]
+            [mount.core :as mount]
+            [app.extend.query :refer [query-routes]]
+            ))
+
+(mount/defstate init-app
+  :start ((or (:init defaults) identity))
+  :stop  ((or (:stop defaults) identity)))
+
+(mount/defstate app
+  :start
+  (middleware/wrap-base
+    (routes
+      (-> #'home-routes
+          (wrap-routes middleware/wrap-csrf)
+          (wrap-routes middleware/wrap-formats))
+      (-> #'query-routes
+          (wrap-routes middleware/wrap-csrf)
+          (wrap-routes middleware/wrap-formats))          
+      (route/not-found
+        (:body
+          (error-page {:status 404
+                       :title "page not found"}))))))
+
